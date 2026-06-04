fe6cbba9f2f5e6a0197ceaf380157bb2b800c82e
Artem <127234969+BalobanArtem@users.noreply.github.com>
2026-06-04 22:02:13 +0200

    Update views.sql

--- topic-10-views/views.sql
+++ topic-10-views/views.sql
@@ -40,3 +40,32 @@ SELECT group_id, name, max_members
 FROM training_groups
 WHERE name LIKE '%children%' AND max_members <= 8
 WITH CHECK OPTION;
+
+                                                                --- Baloban Artem ---
+--- Tasks: ---
+--- 1.Horizontal views — selecting specific columns, for example, a view showing only book titles and authors.
+--- 2.Vertical views — selecting specific rows based on conditions, for example, a view showing only overdue borrowings.
+--- 5.Views that use subqueries, for example, a view showing books with an average rating above 4.
+
+create or replace view coach_view_city as
+Select coach_id, first_name, address
+from coach
+where address = 'Kyiv';
+
+select * from coach_view_city;
+
+create or replace view personal_view_data as
+select c.coach_id, c.first_name as coach_first_name, c.last_name, m.member_id, m.first_name as member_first_name, pt.ends_at
+from coach c inner join personal_training pt on c.coach_id = pt.coach_id inner join members m on pt.member_id = m.member_id
+Where pt.ends_at::date = DATE '2026-06-01';
+
+select * From personal_view_data;
+
+create or replace view count_members_in_coach as
+select c.coach_id, c.first_name, count(pt.member_id) as member_count
+from coach c join personal_training pt on c.coach_id = pt.coach_id
+where is_active = true
+group by c.coach_id, c.first_name
+having count(pt.member_id) > (select avg(cnt) from (select count(member_id) as cnt from personal_training group by coach_id) sub);
+
+select * from count_members_in_coach;
